ef9664d5985314399c909d94e70687c18129c325
Stephagro <stephagro@gmail.com>
2026-04-28 23:00:30 +0200

    Refactor UIContainer class and add new methods
    Updated UIContainer class to include new methods and variables for activity tracking and screen saver functionality.

--- lib/ZuluSCSI_UI_RP2MCU/UIContainer.h
+++ lib/ZuluSCSI_UI_RP2MCU/UIContainer.h
@@ -1,18 +1,23 @@
 #ifndef UICONTAINER_H
 #define UICONTAINER_H
-#include <Adafruit_SSD1306.h>
+
+#include <Arduino.h>
 #include "Screen.h"
 #include "ScreenType.h"
 
 class UIContainer {
 public:
-    UIContainer(Adafruit_SSD1306* display);
-    void init();
-    void tick();
-    void draw();
-    void changeScreen(SCREEN_TYPE type, int parameter = 0);
+    UIContainer(); // Correspond à ta ligne 36
+    void update(); // Correspond à ta ligne 43
+    void activity(); // Correspond à ta ligne 60
+    
 private:
-    Adafruit_SSD1306* _display;
-    Screen* _currentScreen;
+    unsigned long _lastActivityTime;
+    bool _screenSaverActive;
 };
+
+// Fonctions globales appelées dans ton .cpp
+const char* GetScreenName(SCREEN_TYPE type);
+void changeScreen(SCREEN_TYPE type, int index = 0);
+
 #endif
