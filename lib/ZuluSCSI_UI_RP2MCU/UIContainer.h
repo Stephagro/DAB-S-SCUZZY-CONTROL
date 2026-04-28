7488fe147fe9e43dc686c43d4e24c318bd88b333
Stephagro <stephagro@gmail.com>
2026-04-28 22:53:20 +0200

    Refactor changeScreen method to use SCREEN_TYPE

--- lib/ZuluSCSI_UI_RP2MCU/UIContainer.h
+++ lib/ZuluSCSI_UI_RP2MCU/UIContainer.h
@@ -1,8 +1,8 @@
 #ifndef UICONTAINER_H
 #define UICONTAINER_H
-
 #include <Adafruit_SSD1306.h>
 #include "Screen.h"
+#include "ScreenType.h"
 
 class UIContainer {
 public:
@@ -10,11 +10,9 @@ public:
     void init();
     void tick();
     void draw();
-    void changeScreen(int screenType, int parameter = 0);
-
+    void changeScreen(SCREEN_TYPE type, int parameter = 0);
 private:
     Adafruit_SSD1306* _display;
     Screen* _currentScreen;
 };
-
 #endif
